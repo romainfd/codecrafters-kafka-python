95efaf073425c4ffeac5afb0996716eda1dabb79
Romain Fouilland <romain.fd@gmail.com>
2024-09-14 00:36:52 +0200

    Stage 2

--- app/main.py
+++ app/main.py
@@ -1,15 +1,28 @@
-import socket  # noqa: F401
+from socket import create_server
+
+
+class Message:
+    body: bytes
+
+    def __init__(self, body: bytes):
+        self.body = body
+
+    def to_bytes(self):
+        return len(self.body).to_bytes(4, byteorder="big") + self.body
 
 
 def main():
-    # You can use print statements as follows for debugging,
-    # they'll be visible when running tests.
-    print("Logs from your program will appear here!")
+    server = create_server(("localhost", 9092), reuse_port=True)
+    socket, address = server.accept()  # wait for client
 
-    # Uncomment this to pass the first stage
-    #
-    server = socket.create_server(("localhost", 9092), reuse_port=True)
-    server.accept() # wait for client
+    print(f"Client connected: {address}")
+    # Receive data from the client
+    data = socket.recv(1024)
+    print(f"Received data: {data}")
+    correlation_id = 7  # hardcoded for now
+    message = Message(correlation_id.to_bytes(4, byteorder="big"))
+    print(f"Sending message: {message.to_bytes().hex()}")
+    socket.sendall(message.to_bytes())
 
 
 if __name__ == "__main__":
