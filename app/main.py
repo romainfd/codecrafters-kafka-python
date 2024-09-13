83a34a3766c2f9da9e80a5b52eac09bed4021455
Romain Fouilland <romain.fd@gmail.com>
2024-09-14 01:01:33 +0200

    Stage 3

--- app/main.py
+++ app/main.py
@@ -7,10 +7,10 @@ class Message:
 
     def __init__(self, header: bytes, body: bytes):
         self.header = header
-        self.request_api_key = header[:2]
-        self.request_api_version = header[2:4]
-        self.correlation_id = header[4:8]
-        self.client_id = header[8:]
+        self.request_api_key = int.from_bytes(header[:2], byteorder="big")
+        self.request_api_version = int.from_bytes(header[2:4], byteorder="big")
+        self.correlation_id = int.from_bytes(header[4:8], byteorder="big")
+        self.client_id = int.from_bytes(header[8:], byteorder="big")
         self.tagged_fields = ''  # No tagged fields for us
         self.body = body
 
@@ -27,7 +27,7 @@ def main():
     data = socket.recv(1024)
     print(f"Received data: {data}")
     request = Message(data[4:], b'')
-    message = Message(request.correlation_id, b'')
+    message = Message(request.correlation_id.to_bytes(4, byteorder="big"), int(35).to_bytes(2, byteorder="2"))
     print(f"Sending message: {message.to_bytes().hex()}")
     socket.sendall(message.to_bytes())
 
